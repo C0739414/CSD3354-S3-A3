1bd2729bc730a1667efff5f07d452f0039ce9fc0
Gurpinder Kaur <739414@student.tor.loc>
2019-07-10 09:02:05 -0400

    2nd commit

--- Assign3/Assign3/Program.cs
+++ Assign3/Assign3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,30 @@ namespace Assign3
     {
         static void Main(string[] args)
         {
+            var tp = new TextProcessing();
+            tp.Run();
         }
     }
+
+    class TextProcessing
+    {
+        public void Run()
+        {
+            // Open the names file and access the data
+            using (StreamReader file = new StreamReader("U:/Users/739414/Assign3/Assign3/Assign3/names.txt"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+        }
+    }
+
 }
